8f5879741a08d31e9dd60795874a49717bc60df9
PC\lordfred23 <fred.cote23@gmail.com>
2016-12-08 20:55:40 -0500

    nav bar semi fonctionnel

--- Index.php
+++ Index.php
@@ -4,10 +4,18 @@
 <head>
 	<title>Service Informatique Frédéric Côté</title>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="Style/style.css">
+	 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	
+	
 </head>
 
-<body>
+<body class="Body">
 <div>
 	<?php include "nav-bar.php";?>
 </div>
@@ -17,6 +25,6 @@
 
 
 
-
+<script> $(function(){ $('.nav-tabs a:first').tab('show'); }); </script>
 </body>
 </html>
